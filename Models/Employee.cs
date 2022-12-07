3aef24bf6b868e99a345591afdabe92c02d73bc0
wenqi0425 <xiaoc288@gmail.com>
2022-12-07 19:55:44 +0100

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RazorPageUploadImgTest.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageData { get; set; }
+
+        [NotMapped]
+
+
+    }
+}
